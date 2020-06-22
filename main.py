2cfffa288a3dc10f46a148e82171a8f8a6daafcf
shinydog666 <58540850+shinydog666@users.noreply.github.com>
2020-06-22 16:50:22 +0800

    1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pyperclip
+import re
+import sys
+import os
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from pykeyboard import PyKeyboard
+
+question = pyperclip.paste()
+while True:
+    time.sleep(0.1)
+    string = pyperclip.paste()
+    if string != question and string != '':
+        print([string])
+        question = string
+
+time.sleep(0.5)
+    k = PyKeyboard()
+    k.press_key(k.control_key)
+    k.tap_key('v')
+    k.release_key(k.control_key)
