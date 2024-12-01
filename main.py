600fe8d47fcaf11dbfc354bb62bae10990118270
BH3GEI <58540850+BH3GEI@users.noreply.github.com>
2024-12-01 10:10:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 导入所需的库
 import pyperclip
 from pykeyboard import PyKeyboard
 import re
@@ -8,6 +9,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 def autoPaste():
+    # 模拟 Ctrl+V 和回车键
     time.sleep(0.5)
     k = PyKeyboard()
     k.press_key(k.control_key)
@@ -15,7 +17,10 @@ def autoPaste():
     k.release_key(k.control_key)
     k.tap_key(k.enter_key)
 
+# 获取初始剪贴板内容
 question = pyperclip.paste()
+
+# 监控剪贴板变化
 while True:
     time.sleep(0.1)
     string = pyperclip.paste()
